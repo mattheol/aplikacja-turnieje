e893e0119e24fd1b369a89b73a91aab09eb7ff56
Mateusz Olsztyński <matiol1@hotmail.com>
2020-03-29 16:45:57 +0200

    Modify sample data sql

--- backend/app/src/main/resources/data.sql
+++ backend/app/src/main/resources/data.sql
@@ -1,26 +1,38 @@
+drop table invitations;
+drop table tournaments_organizers;
+drop table matches_participants;
+drop table tournaments_participants;
+drop table users;
+drop table matches;
+drop table tournaments;
+
 INSERT INTO users (id, first_name,last_name,gender,birthday,email,login,password)
        VALUES (1,'Jan','Kowalski','M','2000-01-21','jk@wp.pl',null,null),
               (2,'Monika','Nowak','F','2001-03-01','mk@wp.pl',null,null),
               (3,'Kamil','Głuś','M','2001-03-01','kg@wp.pl',null,null);
 
-INSERT INTO tournaments (id,name,tournament_type,is_private,number_of_players,random_bracket,description)
-       VALUES (1,'Football tour','LEAGUE',true,10,false,null),
-              (2,'CS tour','MIXED',true,16,true,null);
+INSERT INTO tournaments (id,name,tournament_type,is_private,number_of_players,random_bracket, enrollment_end,description)
+       VALUES (1,'Turniej 1','LEAGUE',false,10,false, '2020-03-29', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec metus eros, tempor et tempor vel, pharetra lacinia ex.'),
+              (2,'Turniej 2','MIXED',false,16,true,'2020-04-01','Quisque fermentum tortor nec nibh mollis egestas nec non risus.'),
+              (3,'Turniej 3','LEAGUE',false,10,false, '2020-03-29', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec metus eros, tempor et tempor vel, pharetra lacinia ex.'),
+              (4,'Turniej 4','MIXED',false,16,true,'2020-04-01','Quisque fermentum tortor nec nibh mollis egestas nec non risus.');
 
-INSERT INTO matches (id,comment,score,stage,tournament_id)
-       VALUES (1,'Mecz otwarcia',null ,null ,1),
-              (2,'Mecz o wszystko',null ,null ,1),
-              (3,'Mecz o honor',null ,null ,1);
+INSERT INTO matches (id,comment,score,stage,start_time,tournament_id)
+       VALUES (1,'Mecz otwarcia',null ,null , '2020-05-04 17:00', 1),
+              (2,'Mecz o wszystko',null,null ,'2020-05-05 20:00' ,1),
+              (3,'Mecz o honor',null,null ,'2020-04-07 9:30' ,1);
 
 INSERT INTO invitations (id,invitation_message, invitation_time,organizer_id,participant_id,tournament_id)
-       VALUES (1,'Cho zagrac',null,1,2,1 );
+       VALUES (1,'Zapraszam cię na turniej.',null,1,2,1 );
 
 INSERT INTO tournaments_organizers (tournament_id,organizer_id)
-       VALUES (1,1);
+       VALUES (1,1),(2,2);
 
 INSERT INTO tournaments_participants (tournament_id,participant_id)
        VALUES (1,2),
               (1,3);
+              
 INSERT INTO matches_participants (match_id,participant_id)
         VALUES (1,2),
                (1,3);
+
