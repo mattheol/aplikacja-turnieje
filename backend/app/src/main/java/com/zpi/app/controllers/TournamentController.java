2206e5ea8675b86617b50f03aa81f74d10bcba73
Patryk Wydmuch <pvydmuch@gmail.com>
2020-04-07 15:45:11 +0200

    Add start page

--- backend/app/src/main/java/com/zpi/app/controllers/TournamentController.java
+++ backend/app/src/main/java/com/zpi/app/controllers/TournamentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*")
 public class TournamentController {
     private final TournamentService tournamentService;
 
